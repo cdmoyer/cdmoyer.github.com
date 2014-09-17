696810f3a7feb3a8c6d7d55193355c838af1ffb9
Chris Moyer <chris@inarow.net>
2014-09-16 21:56:04 -0400

    Whoa, lots of stuff.

--- feed.js
+++ feed.js
@@ -3,9 +3,9 @@ layout: nil
 ---
 pageapp.ramblings([
 	{% for post in site.posts offset: 0 limit: 10 %}
-    {% if !post.draft %}
+    {% unless post.draft %}
 	{title: "{{ post.title | escape}}", url: "http://blog.inarow.net{{ post.url }}"} ,
-    {% endif %}
+    {% endunless %}
 	{% endfor %}
 	null
 ]);
