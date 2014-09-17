4f37c435924ab800ecb32a8acaca35fd915095ba
Chris Moyer <chris@inarow.net>
2014-09-16 21:18:33 -0400

    another draft attempt

--- feed.js
+++ feed.js
@@ -3,9 +3,9 @@ layout: nil
 ---
 pageapp.ramblings([
 	{% for post in site.posts offset: 0 limit: 10 %}
-    {% unless post.draft %}
+    {% if !post.draft %}
 	{title: "{{ post.title | escape}}", url: "http://blog.inarow.net{{ post.url }}"} ,
-    {% endunless %}
+    {% endif %}
 	{% endfor %}
 	null
 ]);
